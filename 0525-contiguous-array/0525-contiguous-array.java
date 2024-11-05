15e613b83eb6c92f22700f547f107ee0e2884d9f
dot-Deena <149881601+dot-Deena@users.noreply.github.com>
2024-11-05 15:52:49 +0530

    Time: 25 ms (22.36%), Space: 55.2 MB (40.33%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -6,14 +6,14 @@ class Solution {
         int longest = 0;
         
         for(int i=0; i<nums.length; i++){
-            
+            // treat 0 as -1 and add to cumsum
             sum += (nums[i] == 0? -1:1);
             
-            if(sum==0){
+            if(sum==0){ // if sum=0 take all subarray from start to i
                 longest = i+1;
-            }else if(distinct.containsKey(sum)){
+            }else if(distinct.containsKey(sum)){ // if not and its in hash
                 longest = Math.max(longest, i-distinct.get(sum));
-            }else{
+            }else{ // if not 0 and not in hash
                 distinct.put(sum,i);
             }
         }
