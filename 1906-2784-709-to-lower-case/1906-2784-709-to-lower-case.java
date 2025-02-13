166ae18c7475955fb7a00f6aa0d7ece30baa4415
dot-Deena <149881601+dot-Deena@users.noreply.github.com>
2025-02-13 07:04:04 +0530

    Time: 0 ms, Memory: 41.6 MB - LeetHub

---
+++ 1906-2784-709-to-lower-case/1906-2784-709-to-lower-case.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String toLowerCase(String s) {
+        // Scanner sc = new Scanner(System.in);
+        String s1 = s.toLowerCase();
+        return s1;
+    }
+}
